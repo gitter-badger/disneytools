ddc6f5663e8ddbacc452d5c1a04fa2343321a679
helios1989 <vergelbarit35@gmail.com>
2015-10-16 10:22:54 +0000

    added firebase setup and partial crud using firebase

--- app/admin/dashboard/dashboard.js
+++ app/admin/dashboard/dashboard.js
@@ -1,14 +1,61 @@
-'use strict';
+(function (angular) {
+  "use strict";
 
-angular.module('myApp.dashboard', ['ngRoute'])
+  var app = angular.module('myApp.dashboard', ['ngRoute','firebase']);
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/admin_dashboard', {
-    templateUrl: 'admin/dashboard/dashboard.html',
-    controller: 'DashboardController'
-  });
-}])
+  app.controller('DashboardController', ['$scope','$http','Userlist','$firebaseObject', function($scope,$http,Userlist, $firebaseObject) {
+     
+    var ref = new Firebase("https://amber-torch-9068.firebaseio.com/user");
+    var vm = this;
+    vm.myname = 'hello world';
+    $scope.addItem = function(){
+        alert('test');
+        ref.push({"firstname":"vergel",
+                  "middlename":"noda",
+                  "lastname":"barit",
+                  "age":26,
+                  "hobbies":"basketball"
+        })
+    }
+    
+    $scope.removeItem = function(){
+      alert(this.item);
+    }
+   
+    
+  // download the data into a local object
+   $scope.data = $firebaseObject(ref); 
+   
+   console.log(ref);
+   
+    Userlist.getUsers().then(function(asyncUsers) {
+       $scope.users = asyncUsers.user;
+       console.log(asyncUsers);
+       console.log(asyncUsers.user[0])
+    });
+    
+  }]);
+  
+  app.factory('Userlist', function($http) {
+    //temporary while the firebase has error
+    var UserService = {
+        getUsers: function() {
+            // $http returns a 'promise'
+            return $http.get("admin/temporary-data/Users.json").then(function(response) {
+                return response.data;
+            });
+        }
+    };
 
-.controller('DashboardController', [function() {
+    return UserService;
+});
 
-}]);
+  app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/admin_dashboard', {
+      templateUrl: 'admin/dashboard/dashboard.html',
+      controller: 'DashboardController',
+      controllerAs:'DC'
+    });
+  }]);
+
+})(angular);
