ccdf299cc67a80a0548de2e1cc9aff7da53a6183
helios1989 <vergelbarit35@gmail.com>
2015-10-16 03:56:25 +0000

    changes the naming path of view to admin

---
+++ app/admin/admin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.admin', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/admin', {
+    templateUrl: 'admin/admin.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', [function() {
+
+}]);
