0347b1317d5a13cb6596d1013cf4a32d6c9fcf62
Christian Talampas <talampasch@gmail.com>
2015-10-19 04:04:07 +0000

    Cleaning up registration controller

--- app/registration/registration.js
+++ app/registration/registration.js
@@ -13,30 +13,21 @@ angular.module('myApp.registration', ['ngRoute'])
 .controller('RegistrationCtrl', registration);
 
 registration.$inject = [
-  'RegistrationREST',
   'RegistrationModel',
 ];
 
-function registration (RegistrationREST, RegistrationModel) {
+function registration(Registration) {
   var rgs = this;
   rgs.register = register;
-  rgs.loading = RegistrationModel.loading;
+  rgs.loading = Registration.loading;
 
   // TODO make more elegant
-  function register () {
+  function register() {
     var params = {
       username: (!!rgs.username) ? rgs.username : '--unspecified--',
       email: (!!rgs.email) ? rgs.email : '--unspecified--',
       password: (!!rgs.password) ? sha1(rgs.password) : '--unspecified--',
     }
-
-    var call = RegistrationREST.signup(params);
-    RegistrationModel.loading.watch(
-      call.then(function() {
-        console.log('set was successful!');
-      }, function(err) {
-        console.error('error while setting:', err);
-      })
-    );
+    console.log(Registration.sync(params));
   }
 }
